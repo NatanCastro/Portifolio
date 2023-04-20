bf8b1b4fb109c706cafe698723b852b6bc04b669
NatanCastro <castro.natan@outlook.com>
2023-04-20 15:56:50 -0300

    feat: add basic header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
-    <h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse fugiat at
-      impedit mollitia unde animi neque minima molestias hic, nulla error, a
-      eaque quasi illo nobis laborum eum quisquam veritatis.
-    </h1>
+    <>    
+      <Header />
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse fugiat at
+        impedit mollitia unde animi neque minima molestias hic, nulla error, a
+        eaque quasi illo nobis laborum eum quisquam veritatis.
+      </h1>
+</>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <header className="bg-black text-white flex justify-around px-10 py-4">
+      <h1>Lorem, ipsum dolor.</h1>
+      <ul className="flex gap-5">
+        <li>adad</li>
+        <li>adad</li>
+        <li>adad</li>
+      </ul>
+    </header>
+  );
+};
