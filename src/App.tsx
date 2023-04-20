96694b0152d84d1d937d974a974594726d593e0d
NatanCastro <castro.natan@outlook.com>
2023-04-20 15:59:46 -0300

    style(App.tsx): fix indentation and whitespace issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { Header } from "./components/Header";
 
 function App() {
   return (
-    <>    
+    <>
       <Header />
       <h1>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse fugiat at
         impedit mollitia unde animi neque minima molestias hic, nulla error, a
         eaque quasi illo nobis laborum eum quisquam veritatis.
       </h1>
-</>
+    </>
   );
 }
 
